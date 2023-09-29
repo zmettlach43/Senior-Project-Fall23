70a64bd63dc031816641296ae26323317886a97d
zmettlach43 <zachary.mettlach01@utrgv.edu>
2023-09-29 18:02:10 -0500

    Models and more html for pages

--- SeniorProjectApp/models.py
+++ SeniorProjectApp/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class MenuItemCategory(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class MenuItem(models.Model):
+   name = models.CharField(max_length=100)
+   descritpion = models.TextField()
+   price = models.DecimalField(max_digits=10, decimal_places=2)
+   category = models.ForeignKey(MenuItemCategory, on_delete=models.CASCADE)
+
+   def __str__(self):
+       return self.name + " " + self.price
+
+class Menu(models.Model):
+    name = models.CharField(max_length=100)
+    products = models.ManyToManyField(MenuItem)
+
+    def __str__(self):
+        return self.name

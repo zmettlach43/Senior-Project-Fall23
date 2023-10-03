500a9c615213969a8cae1a667b1a74a9924dc0cf
zmettlach43 <zachary.mettlach01@utrgv.edu>
2023-10-03 00:13:07 -0500

    Add model data and displayed it on menu page + more HTML

--- SeniorProjectApp/models.py
+++ SeniorProjectApp/models.py
@@ -4,18 +4,19 @@ from django.db import models
 
 class MenuItemCategory(models.Model):
     name = models.CharField(max_length=100)
+    subcategory = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
 
 class MenuItem(models.Model):
    name = models.CharField(max_length=100)
-   descritpion = models.TextField()
+   addin = models.CharField(max_length=100, blank=True)
    price = models.DecimalField(max_digits=10, decimal_places=2)
    category = models.ForeignKey(MenuItemCategory, on_delete=models.CASCADE)
 
    def __str__(self):
-       return self.name + " " + self.price
+       return self.name
 
 class Menu(models.Model):
     name = models.CharField(max_length=100)

fb78e091b0506e6ba46a3bfd73297b0dde160451
alashaath <63978366+alashaath@users.noreply.github.com>
2023-10-30 20:52:45 -0500

    added the dynamic slider and fixed the google map

--- SeniorProject2023/settings.py
+++ SeniorProject2023/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import os
 
 from pathlib import Path
 
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-^_@4z)o+l_4%y8frfg4x$+^ycs@#@6w3ii%2wurc+_f5^v_zue
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -57,7 +58,7 @@ ROOT_URLCONF = 'SeniorProject2023.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['template'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,7 +119,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static')
+]
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- SeniorProjectApp/admin.py
+++ SeniorProjectApp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import MenuItemCategory, MenuItem, Menu, Cart
+from .models import MenuItemCategory, MenuItem, Menu, Cart, Carausel
 
 # Register your models here.
 
@@ -7,3 +7,4 @@ admin.site.register(MenuItem)
 admin.site.register(MenuItemCategory)
 admin.site.register(Menu)
 admin.site.register(Cart)
+admin.site.register(Carausel)

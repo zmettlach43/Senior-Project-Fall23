a8ba8ed18ea2d7261639f141673f46e715723738
zmettlach43 <zachary.mettlach01@utrgv.edu>
2023-10-30 21:48:46 -0500

    Image Menu

--- SeniorProjectApp/urls.py
+++ SeniorProjectApp/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home', views.home, name="home"),
+    path('home/', views.home, name="home"),
     path('menu', views.menu, name="menu"),
     path('aboutus', views.aboutus, name="aboutus"),
     path('cart', views.cart, name="cart"),
+    path('item_detail/<int:item_id>/', views.item_detail, name='item_detail'),
 ]
--- SeniorProjectApp/views.py
+++ SeniorProjectApp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import MenuItemCategory, MenuItem, Menu, Cart, Carausel
 
 # Create your views here.
@@ -14,6 +14,10 @@ def home(request):
 def menu(request):
     categories = MenuItemCategory.objects.all()
     return render(request, "SeniorProjectApp/menu.html", {'categories': categories})
+
+def item_detail(request, item_id):
+    item = get_object_or_404(MenuItem, id=item_id)
+    return render(request, 'SeniorProjectApp/item_detail.html/', {'item': item})
 
 def aboutus(request):
     return render(request, "SeniorProjectApp/aboutus.html", {})

0e769a11cc987be8b3577ea2fb2f92b06bd5a8ae
zmettlach43 <zachary.mettlach01@utrgv.edu>
2023-11-18 02:24:21 -0600

    add to cart functionallity

--- Cart/cart.py
+++ Cart/cart.py
@@ -4,6 +4,23 @@ class Cart():
 
         cart = self.session.get('session_key')
         if 'session_key' not in request.session:
-            cart = self.session['sesion_key'] = {}
+            cart = self.session['session_key'] = {}
         
         self.cart = cart
+
+    def add(self, item):
+        item_id = str(item.id)
+
+        if item_id in self.cart:
+            self.cart[item_id]['quantity'] += 1
+        else:
+            self.cart[item_id] = {
+                'name': item.name,
+                'price': str(item.price),
+                'quantity': 1
+            }
+
+        self.session.modified = True
+
+    def __len__(self):
+        return len(self.cart)
--- Cart/views.py
+++ Cart/views.py
@@ -1,11 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .cart import Cart
+from SeniorProjectApp.models import MenuItem
+from django.http import JsonResponse
 
 def cart(request):
-    return render(request, "cart/cart.html", {})
+    cart = Cart(request)
+    cart_items = cart.cart.values()
+    name = cart.cart.values
+    total_price = sum(float(item['price']) for item in cart_items)
+
+    return render(request, "cart/cart.html", {'cart_items': cart_items, 'total_price': total_price, 'name': name})
 
 
 def cart_add(request):
-    pass
+    cart = Cart(request)
+
+    if request.POST.get('action') == 'post':
+        item_id = int(request.POST.get('product_id'))
+        item = get_object_or_404(MenuItem, id=item_id)
+
+        cart.add(item=item)
+        cart_quantity = cart.__len__()
+
+        response_data = {'qty': cart_quantity}
+        return JsonResponse(response_data)
+
 
 def cart_delete(request):
     pass

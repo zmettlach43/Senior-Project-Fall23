d4e9218a1f9a62096114fdd80ace470e6f254409
alashaath <63978366+alashaath@users.noreply.github.com>
2023-12-07 23:40:44 -0600

    stripe working

--- SeniorProjectApp/urls.py
+++ SeniorProjectApp/urls.py
@@ -12,6 +12,8 @@ urlpatterns = [
     path('logout/', views.logout_user, name='logout'),
     path('register/', views.register_user, name="register"),
     path('checkout/', views.checkout, name='checkout'),
+    path('success/', views.payment_success, name='payment_success'),
+    path('cancel/', views.payment_cancel, name='payment_cancel'),
     
 ]
 
